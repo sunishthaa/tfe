7985657442a6108b5ff6a56070480320804cbab2
sunishthaa <31092306+sunishthaa@users.noreply.github.com>
2020-11-09 17:08:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
-test
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0dba2cb6798deb6d8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
