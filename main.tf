2de251ac81f12748ecd8b9fbb004e98c17d8aa7d
Sunishthaa Sharma <sunishthaa.sharma@wipro.com>
2022-01-12 11:30:49 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ resource "aws_instance" "web" {
     Name = "secnd"
   }
 }
+
+
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0dba2cb6798deb6d8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "secnd"
+  }
+}
