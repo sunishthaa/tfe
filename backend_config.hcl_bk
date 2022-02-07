69363ba29732cab3bdc5fb308b9717b87ff33e39
Sunishthaa Sharma <sunishthaa.sharma@wipro.com>
2022-02-07 20:54:02 +0530

    test emr

---
+++ backend_config.hcl_bk
@@ -0,0 +1,10 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Sunishthaa_org"
+
+    workspaces {
+      prefix = "aws-"
+    }
+  }
+}
--- main.tf
+++ main.tf
@@ -1,19 +1,10 @@
 provider "aws" {
-
+  
+    access_key = "AKIA267Q5SRMRLT33AF4"
+    secret_key = "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"
     region     = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0dba2cb6798deb6d8"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "secnd"
-  }
-}
-
-
-
 
 resource "aws_instance" "web" {
   ami           = "ami-0dba2cb6798deb6d8"
@@ -23,3 +14,6 @@ resource "aws_instance" "web" {
     Name = "secnd"
   }
 }
+
+
+
